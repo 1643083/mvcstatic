4abebc6da489d57afdd36f00aba0245bb344610b
fabianna perdomo <1643083@senati.pe>
2025-12-30 07:45:08 -0500

    cambios, buscadores completos

--- app/controllers/producto.controller.php
+++ app/controllers/producto.controller.php
@@ -42,6 +42,12 @@ if (isset($_POST['operacion'])) {
         case 'eliminar':
             //algoritmo...
             break;
+        case 'buscarPorId':
+            echo json_encode($producto->buscarPorId($_POST['id']));
+            break;
+        case 'buscarPorMarca':
+            echo json_encode($producto->buscarPorMarca($_POST['marca']));
+            break;
     }
 
 }
